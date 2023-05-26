57c9a5334eca3e59e213141ebfdedc421fb18902
Ovanga Liboire Kevin <kevinliboire@gmail.com>
2023-05-26 16:37:48 +0100

    added user_profiles migration

---
+++ app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserProfileRequest;
+use App\Http\Requests\UpdateUserProfileRequest;
+use App\Models\UserProfile;
+
+class UserProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserProfileRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserProfile $userProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserProfileRequest $request, UserProfile $userProfile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserProfile $userProfile)
+    {
+        //
+    }
+}
