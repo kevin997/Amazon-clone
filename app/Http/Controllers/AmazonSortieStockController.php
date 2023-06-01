dec312543dce80b7dc4379c1b86ecd185c0efc55
madison2016 <arnelmadison@yahoo.fr>
2023-06-01 20:17:24 +0100

    creation sortie stocks migration

---
+++ app/Http/Controllers/AmazonSortieStockController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAmazonSortieStockRequest;
+use App\Http\Requests\UpdateAmazonSortieStockRequest;
+use App\Models\AmazonSortieStock;
+
+class AmazonSortieStockController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAmazonSortieStockRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AmazonSortieStock $amazonSortieStock)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAmazonSortieStockRequest $request, AmazonSortieStock $amazonSortieStock)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AmazonSortieStock $amazonSortieStock)
+    {
+        //
+    }
+}
